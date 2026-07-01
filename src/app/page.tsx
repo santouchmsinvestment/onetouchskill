2e2b003ec33a4c5e1fafd29e310cd2478f6d3e9f
Ripple-Tech <ifeanyiinnocentokoro@gmail.com>
2026-07-01 13:08:55 +0100

    j

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
    <main className="min-h-screen bg-deep-green">
      <Navigation />
-     <Hero />
+     <Hero title="Welcome to OneTouch Skills Development Foundation"
+      subtitle="Helping Hands, Changing Lives." />
        
     </main>
   );
